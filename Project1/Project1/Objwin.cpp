054e6720c79a195b11638956675dbc4facea43b4
qimr98159@mineo.jp <qimr98159@mineo.jp>
2020-10-28 17:15:05 +0900

    クリア画面のバックアップ完了

---
+++ Project1/Project1/Objwin.cpp
@@ -0,0 +1,45 @@
+//使用するヘッダーファイル
+#include "GameL\DrawFont.h"
+#include"GameL\WinInputs.h"
+#include"GameL\SceneManager.h"
+
+
+#include"GameHead.h"
+#include"Objwin.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjWin::Init()
+{
+	m_key_flag = false;
+}
+//アクション
+void CObjWin::Action()
+{
+	//エンターキーを押してシーンタイトルを移行する
+	if (Input::GetVKey(VK_RETURN) == true)
+	{
+		if (m_key_flag == true)
+		{
+			Scene::SetScene(new CSceneTitle());
+			m_key_flag = false;
+		}
+	}
+	else
+	{
+		m_key_flag = true;
+	}
+}
+
+//ドロー
+void CObjWin::Draw()
+{
+	//描画カラー情報
+	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
+
+	//フォントの表示
+	Font::StrDraw(L"You Win!", 270, 200, 50, c);
+	Font::StrDraw(L"エンターキーでタイトル戻る。", 170, 500, 30, c);
+}
