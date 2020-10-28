7d251178afec957cb67ccb1ebb41856373bd9aa7
qimr98159@mineo.jp <qimr98159@mineo.jp>
2020-10-28 17:56:00 +0900

    GaneHead.hの復元完了 コンソールからwindows化にしました

--- Project1/Project1/GameHead.h
+++ Project1/Project1/GameHead.h
@@ -6,6 +6,19 @@ enum OBJ_NAME
 	OBJ_NO_NAME,	//オブジェクト名無し(禁止)
 	//ゲームで使うオブジェクトの名前
 	//OBJ_○○と表記
+	OBJ_WIN,
+	OBJ_CLEAR_BACK,
+	OBJ_CLEAR_VIRUS,
+	OBJ_TITLE,
+	OBJ_GAME_OVER,
+	OBJ_HERO,
+	OBJ_SCENE_MAIN,
+	OBJ_BULLET,
+	OBJ_ENEMY,
+	OBJ_ATTACK_ENEMY,
+	OBJ_BULLET_ENEMY,
+	/*OBJ_SIN_ENEMY,*/
+	OBJ_BOSS_ENEMY,
 };
 //------------------------------------------------
 
@@ -49,14 +62,31 @@ struct UserData
 //------------------------------------------------
 
 //ゲームシーンオブジェクトヘッダ------------------
-
+#include "Objwin.h"
+#include "ObjClearBack.h"
+#include"ObjClearVirus.h"
+#include"ObjTitle.h"
+#include"ObjSceneMain.h"
+#include"CObjBulletEnemy.h"
+#include"ObjBullet.h"
+//#include"CObjSinEnemy.h"
+#include"ObjAttackEnemy.h"
+#include"ObjClearBack.h"
+#include"ObjGameOver.h"
+#include"ObjSceneMain.h"
+#include"ObjHero.h"
+#include"ObjEnemy.h"
+#include "ObjEnemyBoss.h"
 //------------------------------------------------
 
 //ゲームシーンクラスヘッダ------------------------
 #include"winscene.h"
+#include"SceneTitle.h"
+#include"SceneMain.h"
+#include"SceneGameOver.h"
 //-----------------------------------------------
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-#define SET_GAME_START CSceneWin  
+#define SET_GAME_START CSceneTitle
 //-----------------------------------------------
