cc4edc66115ac3db27142c657bbb3c724ade9c99
uedashigeki <72432577+uedashigeki@users.noreply.github.com>
2020-11-09 17:57:59 +0900

    主人公機の調整を行いました

--- Project1/Project1/ObjHero.cpp
+++ Project1/Project1/ObjHero.cpp
@@ -4,26 +4,22 @@
 #include "GameHead.h"
 #include "ObjHero.h"
 #include"GameL\HitBoxManager.h"
+#include "GameL/DrawFont.h"
+
 
 
 //イニシャライズ
 void CObjHero::Init()
 {
+
 	m_hp = 3;
-	m_ka = 1;
+	m_ka = 5;
 	m_x = 6;
 	//当たり判定用hitboxを作成
 	Hits::SetHitBox(this, m_x, m_y, 32, 32, ELEMENT_PLAYER, OBJ_HERO, 100);
-
-	//blockの衝突状態の確認用
-	m_hit_up=false;
-	m_hit_down=false;
-	m_hit_left=false;
-	m_hit_right=false;
 }
 
 //アクション
-/*--------------------主人公の弾------------------------------*/
 void CObjHero::Action()
 {
 	//主人公機の弾丸発射
@@ -38,6 +34,7 @@ void CObjHero::Action()
 			m_f = false;
 		}
 	}
+
 	//主人公機の弾丸発射
 	if (Input::GetVKey('X') == true)
 	{
@@ -63,31 +60,27 @@ void CObjHero::Action()
 	{
 		m_f = true;
 	}
-		
-	}
-	/*-------------------------主人公の移動---------------------*/
 	//主人公機の移動
 	if (Input::GetVKey(VK_RIGHT) == true)
 	{
-		m_x += 6.0f;
+		m_x += 5.0f;
 
 	}
 	if (Input::GetVKey(VK_LEFT) == true)
 	{
-		m_x -= 6.0f;
+		m_x -= 5.0f;
 
 	}
 	if (Input::GetVKey(VK_UP) == true)
 
 	{
-		m_y -= 6.0f;
+		m_y -= 5.0f;
 	}
 	if (Input::GetVKey(VK_DOWN) == true)
 
 	{
-		m_y += 6.0f;
+		m_y += 5.0f;
 	}
-
 	//主人公が領域外に行かない処理
 	if (m_x + 32.0 > 800.0f)
 	{
@@ -110,6 +103,7 @@ void CObjHero::Action()
 	//HITboxの内容を更新
 	CHitBox* hit = Hits::GetHitBox(this); //作成したhitBox更新用の入り口を取り出す
 	hit->SetPos(m_x, m_y);				  //入り口から新しい位置（主人公機の位置）情報に置き換える
+
 
 	//ELEMENT_ENEMYを持つオブジェクトと接触したらライフ1つ減らす
 	if (hit->CheckElementHit(ELEMENT_ENEMY) == true)
@@ -143,35 +137,55 @@ void CObjHero::Action()
 //ドロー
 void CObjHero::Draw()
 {
-/*-------------------主人公ライフ------------------*/
 	float c[4] = { 1.0f,1.0f, 1.0f, 1.0f, };
 
 	wchar_t str[256];
-	swprintf_s(str, L"HP=%d", m_hp);
-	Font::StrDraw(str, 50, 20, 30, c);
-if (m_hp == 3)
-{
-	swprintf_s(str, L"♡♡♡", m_hp);
-	Font::StrDraw(str, 100, 70, 40, c);
-}
-if (m_hp == 2)
-{
-	swprintf_s(str, L"♡♡", m_hp);
-	Font::StrDraw(str, 100, 70, 40, c);
-}
-if (m_hp == 1)
-{
-	swprintf_s(str, L"♡", m_hp);
-	Font::StrDraw(str, 100, 70, 40, c);
-}
-/*--------------------拡散弾使用確認メーター---------*/
-if (m_ka == 5)
-{
-	swprintf_s(str, L"*****", m_ka);
-	Font::StrDraw(str, 40, 30, 60, c);
-}
+	//swprintf_s(str,L"HP=%d", m_hp);
+	//Font::StrDraw(str, 50, 20, 30, c);
 
-	float c[4] = { 1.0f,1.0f, 1.0f, 1.0f, };
+	/*---------HP表示---------------------*/
+	if (m_hp == 3)
+	{
+		swprintf_s(str, L"♡♡♡", m_hp);
+		Font::StrDraw(str, 20, 10, 60, c);
+	}
+	if (m_hp == 2)
+	{
+		swprintf_s(str, L"♡♡", m_hp);
+		Font::StrDraw(str, 20, 10, 60, c);
+	}
+	if (m_hp == 1)
+	{
+		swprintf_s(str, L"♡", m_hp);
+		Font::StrDraw(str, 20, 10, 60, c);
+	}
+	/*-------------------拡散団残りメーター-----------------*/
+	if (m_ka == 5)
+	{
+		swprintf_s(str, L"*****", m_ka);
+		Font::StrDraw(str, 40, 30, 60, c);
+	}
+	if (m_ka == 4)
+	{
+		swprintf_s(str, L"****", m_ka);
+		Font::StrDraw(str, 40, 30, 60, c);
+	}
+	if (m_ka == 3)
+	{
+		swprintf_s(str, L"***", m_ka);
+		Font::StrDraw(str, 40, 30, 60, c);
+	}
+	if (m_ka == 2)
+	{
+		swprintf_s(str, L"**", m_ka);
+		Font::StrDraw(str, 40, 30, 60, c);
+	}
+	if (m_ka == 1)
+	{
+		swprintf_s(str, L"*", m_ka);
+		Font::StrDraw(str, 40, 30, 60, c);
+	}
+
 	RECT_F src;//描画元切り取り位置
 	RECT_F dst;//描画先表示位置
 	//切り取り位置の設定
@@ -185,5 +199,6 @@ if (m_ka == 5)
 	dst.m_right = 32.0f + m_x;
 	dst.m_bottom = 32.0f + m_y;
 
-	Draw::Draw(1, &src, &dst, c, 0.0f);
+	Draw::Draw(0, &src, &dst, c, 0.0f);
+
 }
