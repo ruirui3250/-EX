ae259b437c48e35a53fa9b684600ec486cc48a12
光城 桶谷 <h1202016015@hiratagakuen.onmicrosoft.com>
2020-11-10 17:17:08 +0900

    画像変更しました

--- Project1/Project1/Objwin2.cpp
+++ Project1/Project1/Objwin2.cpp
@@ -39,7 +39,7 @@ void CObjWin2::Draw()
 	//描画カラー情報
 	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
 
-	//フォントの表示
-	Font::StrDraw(L"You Win!", 270, 200, 50, c);
-	Font::StrDraw(L"エンターで次のステージへ!!", 170, 500, 30, c);
+	////フォントの表示
+	//Font::StrDraw(L"You Win!", 270, 200, 50, c);
+	//Font::StrDraw(L"エンターで次のステージへ!!", 170, 500, 30, c);
 }
