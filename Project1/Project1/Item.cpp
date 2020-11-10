10e50fa77e4c38f7ae71bd2bbce75aac825793a4
光城 桶谷 <h1202016015@hiratagakuen.onmicrosoft.com>
2020-11-10 15:49:09 +0900

    GameHead.hにOBJアイテムとアイテム.hを追加しました

--- Project1/Project1/Item.cpp
+++ Project1/Project1/Item.cpp
@@ -7,3 +7,13 @@
 
 //使用するネームスペース
 using namespace GameL;
+
+//イニシャライズ 宣言
+CObjItem::CObjItem(float x,float y)
+{
+	m_x = x;
+	m_y = y;
+
+	//当たり判定用hitboxを作成
+	Hits::SetHitBox(this, m_x, m_y, 32, 32, ELEMENT_PLAYER, OBJ_ITEM, 100);
+}
