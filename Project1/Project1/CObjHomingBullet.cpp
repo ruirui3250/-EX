34d5393668fbe0526548dd69e8da565c71c62b7c
瑠唯 蛭間 <h1202019031@hiratagakuen.onmicrosoft.com>
2020-11-18 09:40:05 +0900

    Revert "HomingBullet変更途中"
    This reverts commit ce300b921ae0ae47061891bbab3e2e67e9b8fa93.

--- Project1/Project1/CObjHomingBullet.cpp
+++ Project1/Project1/CObjHomingBullet.cpp
@@ -38,9 +38,6 @@ void CObjHomingBullet::Init()
 //アクション
 void CObjHomingBullet::Action()
 {
-    //主人公機と誘導弾丸で角度を取る
-    CObjHero* obj = (CObjHero*)Objs::GetObj(OBJ_HERO);
-
     //Resourcesの描写物のRECT
     m_eff = GetBulletEffect(&m_ani, &m_ani_time, m_del, 2);
 
@@ -59,7 +56,8 @@ void CObjHomingBullet::Action()
         return;//消滅処理は、ここでアクションメソッドを終了させる
     }
 
-    
+    //主人公機と誘導弾丸で角度を取る
+    CObjHero* obj = (CObjHero*)Objs::GetObj(OBJ_HERO);
 
     //主人公機が存在する場合、誘導角度の計算する
     if (obj != nullptr)
