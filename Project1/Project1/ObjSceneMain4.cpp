88d0fb841b9efbedcdb8dc3f3567cdc8049ffd09
光城 桶谷 <h1202016015@hiratagakuen.onmicrosoft.com>
2020-12-01 14:22:38 +0900

    4面修正中

--- Project1/Project1/ObjSceneMain4.cpp
+++ Project1/Project1/ObjSceneMain4.cpp
@@ -11,7 +11,7 @@ using namespace GameL;
 //イニシャライズ
 void CObjSceneMain4::Init()
 {
-	m_y1 = 800.0f;
+	m_y1= 800.0f;
 	m_y2= 0.0f;
 }
 //アクション
@@ -23,7 +23,7 @@ void CObjSceneMain4::Action()
 		m_y1 -=800;
 	//背景2の操作
 	m_y2 +=10.0f;
-	if (m_y2 >-800.0f)
+	if (m_y2>-800.0f)
 		m_y2 -= 800;
 	////画面を回転させる
 	//float ar = 170.0f;
@@ -66,19 +66,19 @@ void CObjSceneMain4::Draw()
 	//Draw::Draw(5, &src, &dst, c, 0.0f);
 
 	//背景１を設定描画
-	dst.m_top = 1200.0f - m_y2;
+	dst.m_top = 1200.0f + m_y2;
 	dst.m_left = 1000.0f;
 	dst.m_right = 0.0f;
-	dst.m_bottom = 0.0f - m_y2;
+	dst.m_bottom = 0.0f + m_y2;
 
 	//0番目に登録したグラフィックを描画。
 	Draw::Draw(5, &src, &dst, c, 0.0f);
 
 	//背景2を設定描画
-	dst.m_top = 1200.0f - m_y1;
+	dst.m_top = 1200.0f + m_y1;
 	dst.m_left = 1000.0f;
 	dst.m_right = 0.0f;
-	dst.m_bottom = 0.0f - m_y1;
+	dst.m_bottom = 0.0f + m_y1;
 
 	//0番目に登録したグラフィックを描画。
 	Draw::Draw(5, &src, &dst, c, 0.0f);
