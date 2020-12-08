e324a6d32e2411e391dfa7f96ad39dc1b0e45656
瑠唯 蛭間 <h1202019031@hiratagakuen.onmicrosoft.com>
2020-12-08 09:38:49 +0900

    特に変更していませんが変更変更していると出るのでコミット

--- Project1/Project1/GameHead.h
+++ Project1/Project1/GameHead.h
@@ -169,5 +169,5 @@ struct UserData
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-#define SET_GAME_START  CSceneMain4
+#define SET_GAME_START  CSceneMain
 //-----------------------------------------------
