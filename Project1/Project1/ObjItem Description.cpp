f80b14da88234980c0618c52069664ef61c02532
光城 桶谷 <h1202016015@hiratagakuen.onmicrosoft.com>
2021-01-25 14:23:16 +0900

    説明文の追加

--- Project1/Project1/ObjItem Description.cpp
+++ Project1/Project1/ObjItem Description.cpp
@@ -53,5 +53,6 @@ void CObjItemD::Draw()
 	Font::StrDraw(L"白血球", 0, 160, 30, c);
 	Font::StrDraw(L"白く浮遊している楕円形状のアイテム", 0, 210, 30, c);
 	Font::StrDraw(L"取るとレーザーと拡散弾の補給が出来る。", 0, 240, 30, c);
-	Font::StrDraw(L"エンターでゲーム開始!", 0, 280, 50, c);
+	Font::StrDraw(L"赤い矢印マークはボス登場位置を表しています。", 0, 300, 30, c);
+	Font::StrDraw(L"エンターでゲーム開始!", 0, 400, 50, c);
 }
