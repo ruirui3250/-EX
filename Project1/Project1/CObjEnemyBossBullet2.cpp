47aff323e388ba95c7a9442b84dd8e53c75a31a2
qimr98159@mineo.jp <qimr98159@mineo.jp>
2021-01-26 14:46:43 +0900

    4面の描画処理

--- Project1/Project1/CObjEnemyBossBullet2.cpp
+++ Project1/Project1/CObjEnemyBossBullet2.cpp
@@ -70,13 +70,6 @@ void CObjEnemyBossBullet2::Action()
 		Hits::DeleteHitBox(this);//“G‹@’eŠÛ‚ªŠ—L‚·‚éHitBox‚ðíœ
 		return;
 	}
-	//ŽålŒö‹@object‚ÆÚG‚µ‚½‚ç“G‹@’eŠÛíœB
-	if (hit->CheckObjNameHit(OBJ_SITA_HERO) != nullptr)
-	{
-		m_del = true; //Á–ÅŽÀs
-		hit->SetInvincibility(true);//“–‚½‚è”»’è–³Œø
-		return;
-	}
 	//’eŠÛ‚ÌÚG‚ð’²‚×‚éB
 	if (hit->CheckObjNameHit(OBJ_SITA_HERO) != nullptr)
 	{
